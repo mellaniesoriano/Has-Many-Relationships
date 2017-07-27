764b072237b3ae7c4d6e55a649af4c43d1cb7058
mellaniesoriano <msoriano808@gmail.com>
2017-07-27 00:22:26 -1000

    query #4 complete

--- joins.sql
+++ joins.sql
@@ -8,3 +8,9 @@ SELECT * FROM posts WHERE users_id = 100;
 SELECT users.first_name, users.last_name, posts.*
 FROM posts INNER JOIN users
 ON posts.users_id = users.id WHERE users_id = 200;
+
+-- Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
+SELECT users.first_name, users.last_name, posts.*
+FROM posts INNER JOIN users
+ON posts.users_id = user.id WHERE users.first_name = 'Norene'
+AND users.last_name = 'Schmitt';
