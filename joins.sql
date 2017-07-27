9682feef3609d51270152a18e8fae67f7502530e
mellaniesoriano <msoriano808@gmail.com>
2017-07-27 12:20:16 -1000

    7 and 8 query done

--- joins.sql
+++ joins.sql
@@ -19,3 +19,19 @@ AND users.last_name = 'Schmitt';
 SELECT username FROM users LEFT JOIN posts
 ON posts.users_id = users.id WHERE posts.created_at > '2015-01-01';
 
+-- Create a query to get the post title, post content, and user's username where the user who created the post joined before January 1, 2015
+SELECT posts.title, posts.content, users.username
+FROM users LEFT JOIN posts
+ON users.id = posts.users_id
+WHERE users.created_at > '2015-01-01';
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as 'Post Title'), and the all the comment's fields
+SELECT comments.*, posts.title AS PostTitle
+FROM posts LEFT JOIN comments
+ON posts.id = comments.posts_id;
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015
+SELECT posts.title AS PostTitle, posts.url AS post_url, comments.body AS comment_body
+FROM posts INNER JOIN comments
+ON posts.id = comments.posts_id
+WHERE posts.created_at < '2015-01-01';
