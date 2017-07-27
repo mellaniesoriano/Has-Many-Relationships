d3adec489c1768c630c7e5724283c432676e921a
mellaniesoriano <msoriano808@gmail.com>
2017-07-27 00:02:30 -1000

    query search from posts to get all fields with user id 100 done

--- joins.sql
+++ joins.sql
@@ -1,2 +1,5 @@
 -- Create a query to get all fields from the users table
 SELECT * FROM users;
+
+-- Create a query to get all fields from the posts table where the user_id is 100
+SELECT * FROM posts WHERE users_id = 100;
