1cd5d55c6024ffb5f83be15f04da419044f3629f
mellaniesoriano <msoriano808@gmail.com>
2017-07-26 21:35:39 -1000

    added drop table statement if users table exists, working on creating user table

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,4 +1,10 @@
 DROP USER IF EXISTS has_many_user;
 CREATE USER has_many_user;
 
+DROP DATABASE IF EXISTS has_many_blogs;
 CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+
+);
