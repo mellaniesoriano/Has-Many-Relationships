1eb274b513dbb1f4d34e41faed92fb6436070b64
mellaniesoriano <msoriano808@gmail.com>
2017-07-27 00:26:53 -1000

    #5 query done

--- joins.sql
+++ joins.sql
@@ -12,5 +12,10 @@ ON posts.users_id = users.id WHERE users_id = 200;
 -- Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
 SELECT users.first_name, users.last_name, posts.*
 FROM posts INNER JOIN users
-ON posts.users_id = user.id WHERE users.first_name = 'Norene'
+ON posts.users_id = users.id WHERE users.first_name = 'Norene'
 AND users.last_name = 'Schmitt';
+
+-- Create a query to get usernames from the users table where the user has created a post after January 1, 2015
+SELECT username FROM users LEFT JOIN posts
+ON posts.users_id = users.id WHERE posts.created_at > '2015-01-01';
+
